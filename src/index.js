ccc70f96bf5f12f37a091f8dcd4052b4186a0819
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2022-01-04 21:38:39 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    str = String(n);
+    strReverse = ""
+    for (let i = 0; i < str.length; i++){
+        strReverse = strReverse + str[parseInt(str.length - 1 - i)];
+    }
+    return parseInt(strReverse);
 }
